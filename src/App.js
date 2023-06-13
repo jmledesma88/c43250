ba221107f0aef9424ea4382184b721c64d917695
jmledesma <jmledesma88@hotmail.com>
2023-06-13 09:11:33 +0200

    empiezo a agregar routeo a mi app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import React, {Component} from "react";
+// import React, {Component} from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+// Componentes
 import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 
-class App extends Component {
-    render() {
-        return(
+// Páginas
+
+const App = () => {  
+    return(
+        <Router>
             <div className="App">
                 <NavBar/>
                 <div id="main">
                     <ItemListContainer greeting="¡Bienvenido, disfruta de nuestras ofertas FLASH solo por 48hs!" fontSize="fs-3"/>
                 </div>
+                <Routes>
+                    <Route exact path="/" element={<HomePage />} />
+                    <Route exact path="/about" element={<AboutPage />} />
+                    <Route exact path="/contact" element={<ContactPage />} />
+                </Routes>
             </div>
-        );
-    }
+        </Router>
+    );   
 }
 
 export default App;
